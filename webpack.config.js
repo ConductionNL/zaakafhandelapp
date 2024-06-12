7f06be71796bb359e803e4ae7c1f71cff68349a0
Remko <remko@conduction.nl>
2024-06-12 14:06:17 +0200

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
-const path = require('path')
-const webpackConfig = require('@nextcloud/webpack-vue-config')
+const path = require("path");
+const webpackConfig = require("@nextcloud/webpack-vue-config");
 
-const buildMode = process.env.NODE_ENV
-const isDev = buildMode === 'development'
-webpackConfig.devtool = isDev ? 'cheap-source-map' : 'source-map'
+const buildMode = process.env.NODE_ENV;
+const isDev = buildMode === "development";
+webpackConfig.devtool = isDev ? "cheap-source-map" : "source-map";
 
 webpackConfig.stats = {
-	colors: true,
-	modules: false,
-}
+  colors: true,
+  modules: false,
+};
 
-const appId = 'dsonextcloud'
+const appId = "dsonextcloud";
 webpackConfig.entry = {
-	main: { import: path.join(__dirname, 'src', 'mainScript.js'), filename: appId + '-mainScript.js' },
-}
+  main: {
+    import: path.join(__dirname, "src", "mainScript.js"),
+    filename: appId + "-mainScript.js",
+  },
+};
 
-module.exports = webpackConfig
+module.exports = webpackConfig;
