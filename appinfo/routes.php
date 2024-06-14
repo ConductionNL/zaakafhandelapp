bc92ac741f5e7df0b6dbeb88296188b0c7ec5ff0
Ruben van der Linde <ruben@conduction.nl>
2024-06-14 21:01:01 +0200

    Added additional routes

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -6,5 +6,25 @@ return [
 			'name' => 'page#mainPage',
 			'url' => '/', 'verb' => 'GET'
 		],
+		[
+			'name' => 'zaken#index',
+			'url' => '/zaken', 'verb' => 'GET'
+		],
+		[
+			'name' => 'zaakTypen#index',
+			'url' => '/zaak_typen', 'verb' => 'GET'
+		],
+		[
+			'name' => 'taken#index',
+			'url' => '/taken', 'verb' => 'GET'
+		],
+		[
+			'name' => 'klanten#index',
+			'url' => '/klanten', 'verb' => 'GET'
+		],
+		[
+			'name' => 'contactMomenten#index',
+			'url' => '/contact_momenten', 'verb' => 'GET'
+		],
 	],
 ];
