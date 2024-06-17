480b6b1ec1fe787f5cf20c07df8c3a6290dcb2bf
Ruben van der Linde <ruben@conduction.nl>
2024-06-17 14:49:50 +0200

    typo-fix

--- settings/adminSettings.php
+++ settings/adminSettings.php
@@ -86,7 +86,7 @@ class AdminSettings implements ISettings {
 	 * @return string the section ID, e.g. 'sharing'
 	 */
 	public function getSection() {
-		return 'additional',;
+		return 'additional';
 	}
 
 	/**
