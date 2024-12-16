00f9bc2f852cb1d26e9463bdbb6e5033464094a8
Thijn <thijn@conduction.nl>
2024-12-16 11:48:35 +0100

    added proper details page

--- src/views/zaakTypen/ZaakTypeDetails.vue
+++ src/views/zaakTypen/ZaakTypeDetails.vue
@@ -7,7 +7,7 @@ import { navigationStore, zaakTypeStore } from '../../store/store.js'
 		<div v-if="!loading" id="app-content">
 			<div class="head">
 				<h1 class="h1">
-					{{ zaakTypeStore.zaakTypeItem?.name }}
+					{{ zaakTypeStore.zaakTypeItem?.identificatie }}
 				</h1>
 
 				<NcActions :primary="true" menu-name="Acties">
@@ -30,8 +30,114 @@ import { navigationStore, zaakTypeStore } from '../../store/store.js'
 				</NcActions>
 			</div>
 
-			{{ zaakTypeStore.zaakTypeItem?.summary }}
+			<div class="detailGrid">
+				<div>
+					<h4>Omschrijving:</h4>
+					<span>{{ zaakTypeStore.zaakTypeItem?.omschrijving || '-' }}</span>
+				</div>
+				<div>
+					<h4>Generieke omschrijving:</h4>
+					<span>{{ zaakTypeStore.zaakTypeItem?.omschrijvingGeneriek || '-' }}</span>
+				</div>
+				<div>
+					<h4>Vertrouwelijkheidaanduiding:</h4>
+					<span>{{ zaakTypeStore.zaakTypeItem?.vertrouwelijkheidaanduiding || '-' }}</span>
+				</div>
+				<div>
+					<h4>Doel:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.doel || '-' }}</p>
+				</div>
+				<div>
+					<h4>Aanleiding:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.aanleiding || '-' }}</p>
+				</div>
+				<div>
+					<h4>Toelichting:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.toelichting || '-' }}</p>
+				</div>
+				<div>
+					<h4>Indicatie intern of extern:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.indicatieInternOfExtern || '-' }}</p>
+				</div>
+				<div>
+					<h4>Handeling initiatie:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.handelingInitiator || '-' }}</p>
+				</div>
+				<div>
+					<h4>Onderwerp:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.onderwerp || '-' }}</p>
+				</div>
+				<div>
+					<h4>Handeling behandelaar:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.handelingBehandelaar || '-' }}</p>
+				</div>
+				<div>
+					<h4>Doorlooptijd:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.doorlooptijd || '-' }}</p>
+				</div>
+				<div>
+					<h4>Servicenorm:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.servicenorm || '-' }}</p>
+				</div>
+				<div>
+					<h4>Opschorting en aanhouding mogelijk:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.opschortingEnAanhoudingMogelijk || '-' }}</p>
+				</div>
+				<div>
+					<h4>Verlenging mogelijk:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.verlengingMogelijk || '-' }}</p>
+				</div>
+				<div>
+					<h4>Verlengingstermijn:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.verlengingstermijn || '-' }}</p>
+				</div>
+				<div>
+					<h4>Publicatie indicatie:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.publicatieIndicatie || '-' }}</p>
+				</div>
+				<div>
+					<h4>Publicatietekst:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.publicatietekst || '-' }}</p>
+				</div>
+				<div>
+					<h4>Producten of diensten:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.productenOfDiensten || '-' }}</p>
+				</div>
+				<div>
+					<h4>Selectielijst proces type:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.selectielijstProcestype || '-' }}</p>
+				</div>
+				<div>
+					<h4>Referentieproces:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.referentieproces || '-' }}</p>
+				</div>
+				<div>
+					<h4>Catalogus:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.catalogus || '-' }}</p>
+				</div>
+				<div>
+					<h4>Begin geldigheid:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.beginGeldigheid || '-' }}</p>
+				</div>
+				<div>
+					<h4>Einde geldigheid:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.eindeGeldigheid || '-' }}</p>
+				</div>
+				<div>
+					<h4>Begin object:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.beginObject || '-' }}</p>
+				</div>
+				<div>
+					<h4>Einde object:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.eindeObject || '-' }}</p>
+				</div>
+				<div>
+					<h4>Versiedatum:</h4>
+					<p>{{ zaakTypeStore.zaakTypeItem?.versiedatum || '-' }}</p>
+				</div>
+			</div>
 		</div>
+
 		<NcLoadingIcon v-if="loading"
 			:size="100"
 			appearance="dark"
@@ -111,9 +217,11 @@ h4 {
 
 .detailGrid {
 	display: grid;
-	grid-gap: 24px;
+	grid-gap: 0.5rem;
 	grid-template-columns: 1fr 1fr;
-	margin-block-start: var(--zaa-margin-50);
-	margin-block-end: var(--zaa-margin-50);
+}
+.detailGrid h4 {
+    margin: 0 !important;
+    font-size: initial !important;
 }
 </style>
