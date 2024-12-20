649fc34f054cc26b74748cc79bb2905430374384
Thijn <thijn@conduction.nl>
2024-12-20 15:21:58 +0100

    finished medewerker routes

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -55,6 +55,8 @@ return [
 		['name' => 'taken#page','postfix' => 'details', 'url' => '/taken/{id}', 'verb' => 'GET'],
 		['name' => 'klanten#page','url' => '/klanten', 'verb' => 'GET'],
 		['name' => 'klanten#page','postfix' => 'details', 'url' => '/klanten/{id}', 'verb' => 'GET'],
+		['name' => 'medewerkers#page','url' => '/medewerkers', 'verb' => 'GET'],
+		['name' => 'medewerkers#page','postfix' => 'details', 'url' => '/medewerkers/{id}', 'verb' => 'GET'],
 		['name' => 'berichten#index','url' => '/berichten', 'verb' => 'GET'],
 		// user Settings
 		['name' => 'settings#index','url' => '/settings', 'verb' => 'GET'],
