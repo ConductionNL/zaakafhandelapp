c73ff1636c4f14cb2bb8436dbf5f0642dc00d210
Robert Zondervan <robert@conduction.nl>
2024-12-20 14:59:03 +0100

    Add docblocks

--- lib/Service/MailService.php
+++ lib/Service/MailService.php
@@ -5,14 +5,31 @@ namespace OCA\ZaakAfhandelApp\Service;
 use OCP\IURLGenerator;
 use OCP\Mail\IMailer;
 
+/**
+ * Service class for sending e-mails
+ *
+ * This service sends e-mails when an 'medewerker' field is filled on an object.
+ */
 class MailService
 {
+	/**
+	 * Constructor for MailService.
+	 */
 	public function __construct(
 		private readonly IMailer $mailer,
 		private readonly IURLGenerator $urlGenerator,
 	) {
 	}
 
+	/**
+	 * Sends an e-mail when a task is connected to an employee.
+	 *
+	 * @param array $oldObject The previous version of the object (to check if the field changes)
+	 * @param array $newObject The current version of the object.
+	 *
+	 * @return array The current version of the object.
+	 * @throws \Exception
+	 */
 	public function sendMail(array $oldObject, array $newObject): array
 	{
 		if(isset($newObject['medewerker']) === false) {
